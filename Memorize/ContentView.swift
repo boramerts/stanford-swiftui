742595e37f25593e751361ac9b7e650795919d2f
boramerts <mertbora723@gmail.com>
2023-08-25 17:36:29 +0300

    Added grid view

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,28 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis: [String] = ["🚗", "🚕", "🚙", "🚌", "🚎", "🏎️", "🚓", "🚑", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🚝", "🚄", "🚅", "🚈", "🚂", "🛰️"]
+    @State var emojiCount = 12
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 75))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            Spacer(minLength: 20)
+            HStack{
+                remove.font(.largeTitle)
+                Spacer()
+                VStack{
+                    Text("Change")
+                    Text("Cards")
+                }.foregroundColor(.accentColor)
+                .font(.body)
+                Spacer()
+                add.font(.largeTitle)
+            }
+            
+            .padding(.horizontal)
         }
-        .padding(.horizontal)
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if emojiCount > 1 {
+                emojiCount -= 1
+            }
+        }, label: {
+            Image(systemName: "minus.circle")
+        })
+    }
+    var add: some View {
+        Button(action: {
+            if emojiCount < 19 {
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
     }
 }
 
 struct CardView: View {
-    //var content: String
-    @State var isFaceUp: Bool = false
+    var content: String
+    @State var isFaceUp: Bool = true
     
     var body: some View {
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 25.0)
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 4).foregroundColor(.red)
-                Text("⭐️").font(.largeTitle)
+                shape.strokeBorder(lineWidth: 4).foregroundColor(.red)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill().foregroundColor(.red)
             }
